9ced0cfe873770a2675a22fa89db2019ed50a5c1
Hogor007 <asifpenta@gmail.com>
2022-05-26 17:01:22 +0600

    mongodb

--- index.js
+++ index.js
@@ -30,11 +30,10 @@ function verifyJWT(req, res, next) {
   });
 }
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q4ici.mongodb.net/?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zw2ufgn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+  useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1
 });
 const run = async () => {
   try {
