45d429a77cf850b10483a6090fb60965c0cfb668
Hogor007 <asifpenta@gmail.com>
2022-05-26 16:29:04 +0600

    delete order

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
@@ -29,3 +29,46 @@ function verifyJWT(req, res, next) {
     next();
   });
 }
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q4ici.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+const run = async () => {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    const db = client.db("toolsDB");
+    const toolsCollection = db.collection("toolsCollection");
+    const ordersCollection = db.collection("ordersCollection");
+    const usersCollection = db.collection("usersCollection");
+    const reviewsCollection = db.collection("reviewsCollection");
+    const blogsCollection = db.collection("blogsCollection");
+    const adminsCollection = db.collection("adminsCollection");
+
+
+    //API to delete a order
+    app.delete("/orders/:id", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.headers.email;
+      if (email === decodedEmail) {
+        const id = req.params.id;
+        console.log("id", id);
+        const result = await ordersCollection.deleteOne({
+          _id: ObjectId(id),
+        });
+        res.send(result);
+      } else {
+        res.send("Unauthorized access");
+      }
+    });
+  } finally {
+    // client.close();
+  }
+};
+
+run().catch(console.dir);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
